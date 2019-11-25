1b5c292e9c392f5c61446f421be547b2af174149
BenteTheWagon <33541473+BenteTheWagon@users.noreply.github.com>
2019-11-25 16:24:30 +0100

    Update sorteringsAlgoritmer.py

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -5,21 +5,31 @@ def insertionSort(case):
         curNum = case[i]
         #Current nummer som algoritmen tager = i
         for j in range(i-1, -1, -1):
-            if case [j] > curNum:
+            if case[j] > curNum:
                 #Hvis case [j] er større end curNum (current nummer) også i (index)
-                case [j+1] = case [j]
+                case[j+1] = case[j]
                 #skal der tilføjes +1 casen [j]
+
             else:
-                #ellers
-                case [j+1] = curNum
-                #skal casen [j]+1 = current nummer
+                j+=1
                 break
+            case[j]=curNum
+
+
+
+
+#ellers
+#skal casen [j] tilføjes 1 til current nummer
+
 
 list1 = ['b','j','q','d','k','d','y','x','b','n','m','u','l','a','w','q','e','m']
+list2 = ['9','5','0','5','1','3','4','2','9','3','6','9','l','7','9','2','6','1']
 #Her laver jeg en liste til test af kode
 insertionSort(list1)
+insertionSort(list2)
 #Her siger jeg at InseritonSort skal bruge (list1)
 print (list1)
+print (list2)
 #Her printer den resultatet af den sorterede liste
 
 
